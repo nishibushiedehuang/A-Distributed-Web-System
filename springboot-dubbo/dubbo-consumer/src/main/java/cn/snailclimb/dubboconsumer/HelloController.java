c2c72257408ed9db9c4a45eebbba12de96adc8a5
nishibushiedehuang <38684610+nishibushiedehuang@users.noreply.github.com>
2020-03-08 22:21:12 +0800

    Update HelloController.java

--- springboot-dubbo/dubbo-consumer/src/main/java/cn/snailclimb/dubboconsumer/HelloController.java
+++ springboot-dubbo/dubbo-consumer/src/main/java/cn/snailclimb/dubboconsumer/HelloController.java
@@ -14,11 +14,20 @@ public class HelloController {
 	@Reference
 	private HelloService helloService;
 	
-	@RequestMapping("/hello")
-	public List<Map<String,Object>> hello() {
-        String sql = "select * from user";
-		List<Map<String,Object>> hello = helloService.userList();
-		System.out.println(helloService.userList());
+	@RequestMapping(value="/cinema",params = {"zoneId","keyword"})
+	public List<Map<String,Object>> hello(String zoneId,String keyword){
+		String sql =new String();
+		if(zoneId != null && zoneId.length() > 0 && keyword != null && keyword.length() > 0 ) {
+			sql = "select * from user where zoneId ='"+zoneId+"' and theater_name like '%"+keyword+"%'" ;
+		}else if(zoneId != null && zoneId.length() > 0 ) {
+			sql = "select * from user where zoneId ='"+zoneId+"'" ;
+		}else if(keyword != null && keyword.length() > 0) {
+			sql = "select * from user where theater_name like '%"+keyword+"%'" ;
+		}else {
+			sql = "select * from user";
+		}
+        	System.out.println(helloService.userList(sql));	
+		List<Map<String,Object>> hello = helloService.userList(sql);
 		return hello;
-	}	
+	}
 }
